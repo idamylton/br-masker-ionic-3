830906510b0b8112a5429b503180980b020000a7
Idamylton Garcia <idamylton@gmail.com>
2017-10-08 18:51:51 -0300

    Replace the BrowserModule import with CommonModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/platform-browser';
 
 import { BrMaskerIonic3 } from './directives/brmasker-ionic-3';
 
@@ -13,7 +13,7 @@ import { BrMaskerIonic3 } from './directives/brmasker-ionic-3';
     BrMaskerIonic3
   ],
   imports: [
-    BrowserModule
+    CommonModule
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
